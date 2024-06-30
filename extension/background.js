84423b0cad0497b27becde475504bc7250985d7c
rahulagowda004 <rahulgowda277@gmail.com>
2024-06-30 18:00:35 +0530

    commit

--- extension/background.js
+++ extension/background.js
@@ -1,28 +1,3 @@
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//   if (changeInfo.status === "complete" && tab.active) {
-//     if (tab.url && !tab.url.startsWith("chrome://")) {
-//       // Filter out internal Chrome pages
-//       fetch("http://localhost:5000/url", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({ url: tab.url }),
-//       })
-//         .then((response) => {
-//           if (!response.ok) {
-//             throw new Error("Network response was not ok");
-//           }
-//           return response.json();
-//         })
-//         .then((data) => {
-//           console.log("Response:", data);
-//         })
-//         .catch((error) => console.error("Error:", error));
-//     }
-//   }
-// });
-
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete" && tab.active) {
     if (tab.url && !tab.url.startsWith("chrome://")) {
