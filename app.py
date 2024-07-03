821b1cb07e39daacf3fb0abdec536251f8ba18d9
rahulagowda004 <rahulgowda277@gmail.com>
2024-07-03 16:34:57 +0530

    grand commit

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ def receive_url():
     data = request.get_json()
     url = data.get('url')
     print(url)  # Print the URL only
-    result = classficier.classify_url(url)
-    print(result)
+    result_url = classficier.classify_url(url)
+    print("SITE :",result_url)
     return '', 200  # Return an empty response with status 200
 
 @app.route('/user_input', methods=['POST'])
@@ -32,8 +32,8 @@ def receive_user_input():
     data = request.get_json()
     user_input = data.get('user_input')
     print(user_input)  # Print the user_input only
-    result = classficier.classify_url(user_input)
-    print(result)
+    result_input = classficier.classify_url(user_input)
+    print("Input :",result_input)
     return '', 200  # Return an empty response with status 200
 
 if __name__ == '__main__':
