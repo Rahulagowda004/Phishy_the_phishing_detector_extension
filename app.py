a7761256a600db2f3eff93b1d69f261f174c0193
rahulagowda004 <rahulgowda277@gmail.com>
2024-07-03 19:31:09 +0530

    done with cleaning of url as well just have to push it to main branch GRAND COMMIT

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import warnings
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from urllib.parse import urlparse, parse_qs, urlunparse
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import logging
@@ -18,12 +19,41 @@ log.disabled = True
 
 classifier = URLClassifier()
 
+def clean_url(input_url):
+    # Parse the URL
+    parsed_url = urlparse(input_url)
+    
+    # Extract scheme, netloc (domain), and path
+    scheme = parsed_url.scheme
+    netloc = parsed_url.netloc
+    path = parsed_url.path
+    
+    # Extract and clean query parameters
+    if parsed_url.query:
+        query_params = parse_qs(parsed_url.query)
+        if 'q' in query_params:
+            query = f"q={query_params['q'][0]}"
+        else:
+            query = ""
+    else:
+        query = ""
+    
+    # Reconstruct the cleaned URL
+    cleaned_url = urlunparse((scheme, netloc, path, '', query, ''))
+    
+    return cleaned_url
+
 @app.route('/url', methods=['POST'])
 def receive_url():
     data = request.get_json()
     url = data.get('url')
-    print(url)  # Print the URL only
-    result_url = classifier.classify_url(url)
+    url = clean_url(url)
+    print(url)
+    label_url = classifier.classify_url(url)
+    if label_url == 0 : 
+        result_url = "site is secure"
+    elif label_url == 1 :
+        result_url = "site is not secure"
     print("SITE :", result_url)
     return jsonify(result_url=result_url), 200  # Return the result_url in the response
 
@@ -32,7 +62,11 @@ def receive_user_input():
     data = request.get_json()
     user_input = data.get('user_input')
     print(user_input)  # Print the user_input only
-    result_input = classifier.classify_url(user_input)
+    label_input = classifier.classify_url(user_input)
+    if label_input == 0 :
+        result_input = "input is secure"
+    elif label_input == 1 :
+        result_input = "input is not secure"
     print("Input :", result_input)
     return jsonify(result_input=result_input), 200  # Return the result_input in the response
 
